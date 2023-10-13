3b85ee30577ad610252223eed3816eb145243ac1
karlruutel <karl.ruutel@ametikool.ee>
2023-10-13 10:03:39 +0300

    2

--- tee.py
+++ tee.py
@@ -1,5 +1,7 @@
-numbers = input()
+a, b, c, d = [float(x) for x in input().split()]
 
-a = numbers.split()
+output1 = c * 60
+print(output1)
 
-print(float(a[2])*60)
+if 78 / d + 78 * b / 60 <= 60:
+    print (a * (100 - 22))
